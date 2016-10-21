cbb6d16c8d0cad68066d1450c52dfc910c6123cd
polamjag <s@polamjag.info>
2016-10-22 00:39:48 +0900

    fix selector to catch up latest soundcloud

--- src/js/content.js
+++ src/js/content.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
   if (msg.command === "get_artwork_url") {
-    var node = document.querySelector('#content .fullListenHero__artwork span.sc-artwork');
+    var node = document.querySelector('#content .listenArtworkWrapper .sc-artwork span.sc-artwork');
     var url = window.getComputedStyle(node, false).backgroundImage.slice(4, -1);
     console.log("Detected URL of coverart: " + url);
     sendResponse({url: url});

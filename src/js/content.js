d2e16b327a7bd595ef5224fb2e42b956a52c6bca
polamjag <s@polamjag.info>
2022-11-04 21:58:41 +0900

    format

--- src/js/content.js
+++ src/js/content.js
@@ -1,8 +1,10 @@
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   if (msg.command === "get_artwork_url") {
-    var node = document.querySelector('#content .listenArtworkWrapper .sc-artwork span.sc-artwork');
+    var node = document.querySelector(
+      "#content .listenArtworkWrapper .sc-artwork span.sc-artwork"
+    );
     var url = window.getComputedStyle(node, false).backgroundImage.slice(4, -1);
     console.log("Detected URL of coverart: " + url);
-    sendResponse({url: url});
+    sendResponse({ url: url });
   }
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,51 +1,49 @@
-(function() {
+(function () {
   var urlPatterns = ["https://soundcloud.com/*/*"];
 
   var menuId = chrome.contextMenus.create({
-    type: 'normal',
-    id: 'main',
+    type: "normal",
+    id: "main",
     documentUrlPatterns: urlPatterns,
-    title: "Open this tracks's coverart in new tab"
+    title: "Open this tracks's coverart in new tab",
   });
 
   chrome.contextMenus.create({
-    type: 'normal',
-    id: '500x500',
-    parentId: 'main',
+    type: "normal",
+    id: "500x500",
+    parentId: "main",
     documentUrlPatterns: urlPatterns,
-    title: '500 x 500 (Same as in this page\'s one)',
+    title: "500 x 500 (Same as in this page's one)",
   });
 
   chrome.contextMenus.create({
-    type: 'normal',
-    id: 'original',
-    parentId: 'main',
+    type: "normal",
+    id: "original",
+    parentId: "main",
     documentUrlPatterns: urlPatterns,
-    title: 'Original File',
+    title: "Original File",
   });
 
-  chrome.contextMenus.onClicked.addListener(
-    function(e, tab) {
-      chrome.tabs.sendMessage(
-        tab.id,
-        { command: "get_artwork_url" },
-        function(res) {
-          if (res && res.url) {
-            var url = res.url.slice(1, -1);
-            switch (e.menuItemId) {
-              case 'main':
-              case 'original':
-                var url = url.replace('t500x500.jpg', 'original.jpg');
-                break;
-              case '500x500':
-                break;
-            }
-            chrome.tabs.create({ url: url });
-          } else {
-            window.alert('No jacket image detected');
+  chrome.contextMenus.onClicked.addListener(function (e, tab) {
+    chrome.tabs.sendMessage(
+      tab.id,
+      { command: "get_artwork_url" },
+      function (res) {
+        if (res && res.url) {
+          var url = res.url.slice(1, -1);
+          switch (e.menuItemId) {
+            case "main":
+            case "original":
+              var url = url.replace("t500x500.jpg", "original.jpg");
+              break;
+            case "500x500":
+              break;
           }
+          chrome.tabs.create({ url: url });
+        } else {
+          window.alert("No jacket image detected");
         }
-      );
-    }
-  );
+      }
+    );
+  });
 })();
